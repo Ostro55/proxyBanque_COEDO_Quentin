b09a16bdb218afcc947665fa873977c08875d766
Coedo Quentin <quentin.coedo@epita.fr>
2025-11-20 12:49:07 +0100

    feat: Added draft of a ClientDto

---
+++ proxyBanque_Quentin_Coedo/src/main/java/org/formation/proxyBanque/Dto/ClientDto.java
@@ -0,0 +1,9 @@
+package org.formation.proxyBanque.Dto;
+
+public class ClientDto {
+
+    // Not implemented Dto but would be used to transfer client data while preserving its anonymity
+    
+    private Long id;
+    private String nom;
+}
